5cf79563f34ed25f4cf19636785340236a13c11c
Vương Trần Ngọc <Vng Trn Ngc>
2016-10-29 14:03:29 +0700

    fix login 2

--- app/src/main/java/com/example/sherman/sbook/activities/LoginActivity.java
+++ app/src/main/java/com/example/sherman/sbook/activities/LoginActivity.java
@@ -7,12 +7,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.example.sherman.sbook.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by Admin on 10/29/2016.
  */
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference UserRef;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,9 +30,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.btnLogin) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
+            if (checkValid())
+                openMainActivity();
         }
     }
+
+    private boolean checkValid() {
+
+
+        return false;
+    }
+
+    private void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
