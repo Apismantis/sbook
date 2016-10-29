8e589f9effc8a788811b1deb27d8ad6bcbc93149
Apismantis <Nguyenvantuan.itus@gmail.com>
2016-10-29 15:12:40 +0700

    Fix login

--- app/src/main/java/com/example/sherman/sbook/activities/LoginActivity.java
+++ app/src/main/java/com/example/sherman/sbook/activities/LoginActivity.java
@@ -1,44 +1,72 @@
 package com.example.sherman.sbook.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.sherman.sbook.R;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-/**
- * Created by Admin on 10/29/2016.
- */
-
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference UserRef;
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
 
-        findViewById(R.id.btnLogin).setOnClickListener(this);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.btnLogin) {
-            if (checkValid())
-                openMainActivity();
+    private void login() {
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        String user = getUser(username, password);
+
+        if (!user.equals("")) {
+            SharedPreferences sp = getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("userId", user);
+            editor.apply();
+
+            openMainActivity();
+        } else {
+            Toast.makeText(getApplicationContext(), "Username or password is wrong. Please try again!", Toast.LENGTH_SHORT).show();
         }
     }
 
-    private boolean checkValid() {
+    private String getUser(String username, String password) {
+        if (!username.equals("") && !password.equals("")) {
+            if (username.equals("testuser1"))
+                return "01vKuHSGJTV4Lx9sswhuWc30BC12";
+            else if (username.equals("testuser2"))
+                return "NDJah5ROVaRkrunxSQDaEX7h7Ph2";
+            return "";
+        }
 
-
-        return true;
+        return "";
     }
 
     private void openMainActivity() {
@@ -46,6 +74,4 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         startActivity(intent);
         finish();
     }
-
-
 }
