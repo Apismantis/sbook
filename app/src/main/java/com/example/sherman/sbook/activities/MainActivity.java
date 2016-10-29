f9107aaac74d1c53763ab29442fc8846a02a11f0
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-10-29 17:55:32 +0700

    Add notify service

--- app/src/main/java/com/example/sherman/sbook/activities/MainActivity.java
+++ app/src/main/java/com/example/sherman/sbook/activities/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.sherman.sbook.activities;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.sherman.sbook.R;
 import com.example.sherman.sbook.constants.Constants;
+import com.example.sherman.sbook.constants.Database;
+import com.example.sherman.sbook.fragments.NotifyBookDialogFragment;
+import com.example.sherman.sbook.services.NotifyService;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
@@ -18,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         initImageLoader();
 
-        Intent intent = new Intent(this, BookDetailActivity.class);
-        intent.putExtra(Constants.bookId, "hp04J1ddxsgpfVscwqpzM2Kz48r2");
-        startActivity(intent);
+        startService(new Intent(this, NotifyService.class));
+//        FragmentManager fm = getSupportFragmentManager();
+//        NotifyBookDialogFragment notifyBookDialogFragment = new NotifyBookDialogFragment();
+//        notifyBookDialogFragment.show(fm, "shoemt");
+
     }
 
     private void initImageLoader() {
