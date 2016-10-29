6e10519ed63abafce0780ab98e1a1011085bf8b4
ShermanDiep <mhung8486@gmail.com>
2016-10-29 10:23:47 +0700

    add recyclerview

---
+++ app/src/main/java/com/example/sherman/sbook/BookRecyclerViewAdapter.java
@@ -0,0 +1,8 @@
+package com.example.sherman.sbook;
+
+/**
+ * Created by Sherman on 10/29/2016.
+ */
+
+public class BookRecyclerViewAdapter extends RecyclerView.Adapter<BookViewHolders> {
+}
---
+++ app/src/main/java/com/example/sherman/sbook/BookViewHolders.java
@@ -0,0 +1,7 @@
+package com.example.sherman.sbook;
+
+/**
+ * Created by Sherman on 10/29/2016.
+ */
+public class BookViewHolders {
+}
