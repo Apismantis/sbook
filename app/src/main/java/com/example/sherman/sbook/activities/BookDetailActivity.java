62eabe83d45f55a48162d16224a866fb36993e3c
Ke Nguyen Phu <kenguyen276@gmail.com>
2016-10-29 10:17:59 +0700

    Structure app folder

--- app/src/main/java/com/example/sherman/sbook/activities/BookDetailActivity.java
+++ app/src/main/java/com/example/sherman/sbook/activities/BookDetailActivity.java
@@ -1,13 +1,15 @@
-package com.example.sherman.sbook;
+package com.example.sherman.sbook.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.sherman.sbook.R;
+
+public class BookDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_book_detail);
     }
 }
---
+++ app/src/main/java/com/example/sherman/sbook/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.sherman.sbook.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sherman.sbook.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
